a45d9f243d6922fbd7fcdede6d48c40ad9fe72f3
Martijn Nieuwenhuizen <mnieuwenhuizen@mirabeau.nl>
2020-06-14 13:11:10 +0200

    added event listener to media items

---
+++ src/components/image/Image.js
@@ -0,0 +1,24 @@
+export default class Image {
+  constructor(element, options) {
+    this._element = element
+    this._options = options
+
+    this._handleClick = this._handleClick.bind(this)
+
+    this._cacheHTMLElements()
+    this._addEventListeners()
+  }
+
+  _cacheHTMLElements() {
+    this._trigger = this._element.querySelector(this._options.triggerSelector)
+    this._target = this._element.querySelector(this._options.targetSelector)
+  }
+
+  _addEventListeners() {
+    this._trigger.addEventListener('click', this._handleClick)
+  }
+
+  _handleClick(event) {
+    // open video
+  }
+}
